88d3a5824c03babb32383f267507da85c6f74a1b
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-01-01 20:17:02 +0530

    rest cntroller for movie catalog

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -0,0 +1,13 @@
+package io.javabrains.moviecatalogservice.resource;
+
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+
+    public List<CatalogItem> getCatalog(String userId) {
+
+    }
+}
