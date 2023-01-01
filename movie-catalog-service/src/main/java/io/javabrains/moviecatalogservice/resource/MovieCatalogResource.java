3c9aca22720315aa5b3e0fda981f01f931942321
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-01-01 20:19:49 +0530

    added movie catalog model

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -4,6 +4,8 @@ package io.javabrains.moviecatalogservice.resource;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MovieCatalogResource {
 
