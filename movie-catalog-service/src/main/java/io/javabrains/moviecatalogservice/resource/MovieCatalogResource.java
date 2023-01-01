1e9486e31f44867b68866ed3048719117438ba19
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-01-01 20:23:17 +0530

    added get catalog controller

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -2,14 +2,22 @@ package io.javabrains.moviecatalogservice.resource;
 
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @RequestMapping("/{userID}")
 
     public List<CatalogItem> getCatalog(String userId) {
 
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );
     }
 }
